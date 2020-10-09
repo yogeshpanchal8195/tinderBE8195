6dd5dc2cdc5b900e0661efbd0a2c9776a1e6b1ab
Yogesh Panchal <yogesh.panchal@infoedge.com>
2020-10-09 13:39:05 +0530

    changes of mongo db url & schema

--- index.js
+++ index.js
@@ -9,7 +9,9 @@ const port = process.env.PORT || 8000;
 app.use(express.json());
 app.use(cors());
 
-mongoose.connect("mongodb+srv://admin:V5zau8EkOWP66clj@cluster0.vqzcn.mongodb.net/tinderDB?retryWrites=true&w=majority", {
+const connectionUrl = "mongodb+srv://admin:V5zau8EkOWP66clj@cluster0.vqzcn.mongodb.net/tinderDB?retryWrites=true&w=majority"
+
+mongoose.connect(connectionUrl, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useUnifiedTopology: true
@@ -19,27 +21,66 @@ app.get('/', (req, res) => {
     res.status(200).send("HELLO YOGESH");
 })
 
-app.get('/tinder/users', (req, res) => {
-    Card.find((err, data) => {
+app.post('/tinder/fetchUsers', (req, res) => {
+    console.log("REQQQ", req.body)
+    const query = { gender: req.body.showMe, displayName: { $ne: req.body.displayName } };
+    if (!req.body.showMe) {
+        delete query.gender
+    }
+    Card.find(query, (err, data) => {
         if (err) {
-            console.log("ERR",err);
+            console.log("ERR", err);
             res.status(500).send(err);
         } else {
+            console.log("DATAA", data)
             res.status(200).send(data);
         }
     });
 })
 
-app.post('/tinder/users', (req, res) => {
+// app.post('/tinder/users', (req, res) => {
+//     const dbCard = req.body;
+//     Card.create(dbCard, (err, data) => {
+//         if (err) {
+//             console.log("ERR", err);
+//             res.status(500).send(err);
+//         } else {
+//             res.status(200).send(data);
+//         }
+//     });
+// })
+
+app.put('/tinder/users', (req, res) => {
     const dbCard = req.body;
-    Card.create(dbCard, (err, data) => {
+    var myquery = { uid: dbCard.uid };
+    var newvalues = { $set: dbCard };
+    Card.findOne({ uid: dbCard.uid }, (err, data) => {
         if (err) {
-            console.log("ERR",err);
+            console.log("ERR", err);
             res.status(500).send(err);
         } else {
-            res.status(200).send(data);
+            if (data) {
+                Card.updateOne(myquery, newvalues, (err, data) => {
+                    if (err) {
+                        console.log("ERR", err);
+                        res.status(500).send(err);
+                    } else {
+                        console.log("DATTTAAAAAA", data);
+                        res.status(200).send(req.body);
+                    }
+                })
+            } else {
+                Card.create(dbCard, (err, data) => {
+                    if (err) {
+                        console.log("ERR", err);
+                        res.status(500).send(err);
+                    } else {
+                        res.status(200).send(data);
+                    }
+                });
+            }
         }
-    });
+    })
 })
 
 app.listen(port, () => console.log(`Listening to port ${port}`))
--- tinderCard.js
+++ tinderCard.js
@@ -1,9 +1,39 @@
 const mongoose = require('mongoose');
 
+// const likeDislikeSchema = mongoose.Schema({
+//     photoURL:String,
+//     uid:String,
+//     displayName:String,
+//     location:String,
+//     age:Number,
+//     status : Number 
+// })
+
 const cardSchema = mongoose.Schema({
-    name:String,
-    url:String
+    photoURL:String,
+    uid:String,
+    displayName:String,
+    email:String,
+    location:String,
+    age:Number,
+    gender:String,
+    showMe:String,
+    UserList:Array
+    // UserList:[
+    //     {
+    //         photoURL:String,
+    //         uid:String,
+    //         displayName:String,
+    //         location:String,
+    //         age:Number,
+    //         status : Number  
+    //         //  -1 disliked   1 liked & send request to update the other person userlist
+    //         //   0 noaction   2 mutually liked & send request to update the other person userlist
+    //     }
+    // ]
 })
+
+
 
 const Card=mongoose.model('cardsTable',cardSchema)
 module.exports = Card;
